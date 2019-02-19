e31069eda73b9aaa5f652ed432cb634025db9c12
AvansMartijn <38460512+AvansMartijn@users.noreply.github.com>
2019-02-19 13:16:13 +0100

    Added Auth

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
-class HomeController extends Controller
+class PagesController extends Controller
 {
     //
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.index');
 });
 
-
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
