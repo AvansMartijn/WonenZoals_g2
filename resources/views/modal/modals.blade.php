06242eca8d316d8f838d572a7773e56b977bd81f
Lars Kruijssen <lmapkrui@avans.nl>
2019-03-13 03:22:35 +0100

    Styled Modals

--- resources/views/modal/modals.blade.php
+++ resources/views/modal/modals.blade.php
@@ -1,35 +1,31 @@
-
-<div class="modal fade lg" id="auke" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+<div class="modal fade lg" id="auke" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">Auke</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
             <div class="modal-body">
-                <img class="img-thumbnail" src="{{ asset('img/Auke.jpg') }}">
-                <p>
-                    Ik ben Auke, geboren op 24 november 1995. Ik woon momenteel met mijn vader en moeder in ’s-Hertogenbosch. Verder heb ik nog een oudere zus die net zoals wij ook in 's-Hertogenbosch woont.
+                
+                <h1 class="modal-Name font-weight-bold">Auke</h5>
 
+                <p class="text-readable">
+                  <img class="modal-picture" src="{{ asset('img/Auke.jpg') }}">
+                    Ik ben Auke, geboren op 24 november 1995. Ik woon momenteel met mijn vader en moeder in ’s-Hertogenbosch. Verder heb ik nog een oudere zus die net zoals wij ook in 's-Hertogenbosch woont.
+                    <br><br>
                     Ik heb al sinds mijn geboorte een lichamelijke beperking, cerebrale parese. Cerebrale parese (CP) is een houding- en bewegingsstoornis die veroorzaakt wordt door beschadiging van de hersenen.
-                    
+                    <br><br>
                     Ik ben een sportieve jongen en mijn favoriete sporten zijn hockey, voetballen, atletiek en fitnessen. Ik speel LG-hockey bij HC Den Bosch, zie foto. Iedere woensdag trainen we en twee keer in de maand spelen we een competitiewedstrijd. In het seizoen 2013-2014 en 2014-2015 zijn we Landskampioen geworden.
-                    
+                    <br><br>
                     Ook vind ik het leuk om met vrienden af te spreken, gezellig samen iets drinken en kletsen over voetbal. Ik heb een seizoenskaart van FC Den Bosch zodat ik alle thuiswedstrijden kan bekijken.
-                    
+                    <br><br>
                     Verder luister ik veel naar muziek en speel ik graag Playstation. Ook ben ik regelmatig op Facebook te vinden. Lekker chatten met mijn vrienden.
-                    
+                    <br><br>
                     Verder ben ik dol op onze hond Nelis. Nelis is een hele lieve zwarte Labradoodle. 
-                    
+                    <br><br>
                     In 2015 heb ik de mytylschool Gabriel in ’s-Hertogenbosch verlaten. Momenteel werk ik 4 dagen in de week bij de buurtsuper in Maliskamp. Ik heb het daar heel goed naar m'n zin, veel afwisseling in werkzaamheden en veel contact met klanten.
-                    
+                    <br><br>
                     Daarna wil ik graag begeleid gaan wonen in of rondom ’s-Hertogenbosch. Als het maar in een straal van 5 km rond de Sint Jan is. Gezellig samen chillen met mijn vrienden. Ik ken de meeste al jaren via school en het lijkt me erg gezellig om samen met nog meer vriend(inn)en een woonvoorziening op te richten.
                 </p>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Sluit</button>
+              <button type="button" class="btn btn-primary" data-dismiss="modal">Sluit</button>
             </div>
           </div>
         </div>
@@ -38,28 +34,25 @@
       <div class="modal fade" id="stef" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
             <div class="modal-dialog" role="document">
               <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLongTitle">Stef</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
                 <div class="modal-body">
-                    <img class="img-thumbnail" src="{{ asset('img/Stef.jpg') }}">
-                    <p>
+
+                  <h1 class="modal-Name font-weight-bold">Stef</h5>
+
+                    <p class="text-readable">
+                      <img class="modal-picture" src="{{ asset('img/Stef.jpg') }}">
                         Ik ben Stef, geboren op 5 mei 1996. Geboren en getogen (zoals ze wel eens zeggen) in Ammerzoden. Ik woon daar met mijn vader en moeder en mijn oudere broer Geert.
-
+                        <br><br>
                         Ik ben geboren met Spina Bifida en Hydrocephalus waardoor ik in een rolstoel zit. Ik kan niet lopen maar praten kan ik als de beste. Vooral van mijn favoriete hobby voetbal, zowel van het Nederlandse als het buitenlandse voetbal, weet ik heel veel te vertellen. Ik speel zelf ook nog rolstoeltennis. Ook doe ik nog wat aan mijn conditie bij van Lith Healthness in Hintham.
-
+                        <br><br>
                         Toen ik ongeveer 3,5 jaar was ben ik naar Mytylschool Gabriël gegaan. In juli 2015 heb ik afscheid genomen van school. Dit was best wel een grote stap maar gelukkig heb ik een hele leuke dagbesteding gevonden bij Werkboerderij Buitengewoon in Berlicum, kijk maar eens op  www.werkboerderijbuitengewoon.nl . Elke dag is hier een feestje!
-
+                        <br><br>
                         Ik ga ook heel graag op stap, voetbal kijken, naar een festival met de mensen van Stichting Typisch en ook ben ik al twee keer op vakantie geweest met Dennis en Luuk, onder begeleiding van de mensen van Mundorado Reizen.
-
+                        <br><br>
                         Veel van de mede-bewoners ken ik al vanaf de tijd dat ik op de mytylschool zat. Onze ouders zouden het heel fijn vinden als wij later bij elkaar in het zelfde huis zouden kunnen wonen en waar we dan ook heel veel plezier kunnen beleven met zijn allen. Niet te ver bij Ammerzoden vandaan zodat zij toch altijd snel bij mij kunnen zijn. Belangrijk voor mij is wel dat ik ook mijn eigen ruimte daar heb en dat ik goede liefdevolle verzorging en begeleiding krijg, want die heb ik wel altijd van anderen nodig.
                     </p>
                     </div>
                 <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Sluit</button>
+                  <button type="button" class="btn btn-primary" data-dismiss="modal">Sluit</button>
                 </div>
               </div>
             </div>
@@ -68,27 +61,28 @@
           <div class="modal fade" id="Isabelle" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                   <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLongTitle">Isabelle</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
                     <div class="modal-body">
-                        <img class="img-thumbnail" src="{{ asset('img/Isabel.jpg') }}">
-                        <p>
+
+                        <h1 class="modal-Name font-weight-bold">Isabelle</h5>
+
+                        <p class="text-readable">
+                            <img class="modal-picture" src="{{ asset('img/Isabel.jpg') }}">
                             Ik ben Isabelle, geboren op 8 augustus 1998.
+                            <br><br>
                             Ik loop stage bij een zorginstelling, waar ik in de horeca en schoonmaak werk. Heb het erg naar mijn zin en werk met leuke collega's.
                             Onlangs hebben we nieuwe bedrijfskleding gehad, waar ik heel blij mee ben.
+                            <br><br>
                             Mijn hobby's zijn muziek luisteren en zingen. En ik zit op toneel.
+                            <br><br>
                             Verder vind ik het altijd gezellig om ergens een "bakkie" te doen, op een terrasje, café en op visite....
+                            <br><br>
                             Ik hoop dat mijn appartement mooi en gezellig wordt ingericht. En dat het ook mijn plekkie wordt. Dat we met ons allen voor een huiselijke sfeer zorgen.
                             En, dat je mag zijn wie je bent, houd je van drukte is dat goed en houd je van rust is dat ook goed.
                             Het echt een voorstelling maken hoe zelfstandig wonen zal zijn, is nog moeilijk. Maar dit zal geleidelijk duidelijk worden.
                         </p>
                     </div>
                     <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Sluit</button>
+                      <button type="button" class="btn btn-primary" data-dismiss="modal">Sluit</button>
                     </div>
                   </div>
                 </div>
@@ -97,26 +91,21 @@
               <div class="modal fade" id="nieuws" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                       <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title" id="exampleModalLongTitle">nieuws</h5>
-                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                          </button>
-                        </div>
                         <div class="modal-body">
-                            <img class="img-thumbnail" src="{{ asset('img/News.jpeg') }}" alt="">
-                            <p>
-                                <h2>Woonzorgappartementen voor jongeren op plaats oude school Mozartsingel</h2>
+
+                            <p class="text-readable">
+                                <img class="modal-picture" src="{{ asset('img/News.jpeg') }}" alt="">
+                                <h2 class="font-weight-bold">Woonzorgappartementen voor jongeren op plaats oude school Mozartsingel</h2>
                                 DEN BOSCH – De oude nutsschool aan de Mozartsingel in Den Bosch gaat plaats maken voor maximaal 18 woonzorgappartementen voor jongeren tussen de 20 en de 35 jaar met een lichte beperking.
-
+                                <br><br>
                                 Het plan is een initiatief van BrabantWonen en ontwikkeld na diverse gesprekken met omwonenden. De jongeren komen er in de toekomst in groepsverband te wonen, waarbij elke jongere een eigen appartement heeft in een van de twee bouwlagen. Het complex heeft twee gemeenschappelijke ruimten, onder meer om te eten. Op het terrein komen elf parkeerplaatsen.
-
-                                <h2>Professionele begeleiding</h2>
+                                <br><br>
+                                <h2 class="font-weight-bold">Professionele begeleiding</h2>
                                 In het complex is straks 24 uur per dag zorg beschikbaar, is er altijd professionele begeleiding en is er ’s nachts altijd een slaapwacht aanwezig. Overdag verblijven de jongeren met een lichte verstandelijke of lichamelijke beperking niet in het gebouw, maar is er altijd dagbesteding of werk buitenshuis.
                             </p>
                         </div>
                         <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Sluit</button>
+                          <button type="button" class="btn btn-primary" data-dismiss="modal">Sluit</button>
                         </div>
                       </div>
                     </div>
