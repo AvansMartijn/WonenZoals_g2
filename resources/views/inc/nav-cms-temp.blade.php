3e5c1e1f1a0dc9a6fe5956ad5e93e0aab34e753d
Lars Kruijssen <lmapkrui@avans.nl>
2019-03-13 04:07:33 +0100

    Fix

--- resources/views/inc/nav-cms-temp.blade.php
+++ resources/views/inc/nav-cms-temp.blade.php
@@ -20,7 +20,7 @@
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                         <!-- dashboard link in the dropdown-->
-                        <a class="dropdown-item" href="/dashboard">Dashboard</a>
+                        <a class="dropdown-item" href="/">Home</a>
 
                         <a class="dropdown-item" href="{{ route('logout') }}"
                             onclick="event.preventDefault();
