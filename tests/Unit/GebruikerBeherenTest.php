c730d86dbc2a359c9ea44e255a42766b3dfef353
Ivo <ivovandaalen22@gmail.com>
2019-04-06 20:14:18 +0200

    Create GebruikerBeherenTest.php

---
+++ tests/Unit/GebruikerBeherenTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class GebruikerBeherenTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_beheerder_can_view_users(){
+        $user = new User([
+            'role' => 'Beheerder'
+        ]);
+
+        $response = $this->actingAs($user)->get('/gebruikers');
+        $response->assertViewIs('dashPages.dashGebruikers');
+    }
+
+//    public function test_resident_cannot_view_users(){
+//        $user = new User([
+//            'role' => 'Bewonder'
+//        ]);
+//
+//        $response = $this->actingAs($user)->get('/gebruikers');
+//        $response->assertViewIs('dashPages.dashGebruikers');
+//    }
+}
