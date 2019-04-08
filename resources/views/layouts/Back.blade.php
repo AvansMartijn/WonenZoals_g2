9cef4cf2e8ec8789be665709227f35c75efe39d8
Lars Kruijssen <lmapkrui@avans.nl>
2019-04-08 16:53:37 +0200

    fixed bg color on front page

--- resources/views/layouts/Back.blade.php
+++ resources/views/layouts/Back.blade.php
@@ -26,7 +26,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
 </head>
-<body>
+<body class="bg-grey">
     <div id="app">
         @include('inc.Navigation.Back_Nav')
         @include('inc.messages')
