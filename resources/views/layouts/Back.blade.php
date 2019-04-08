140099cb4b34508d43b1b9ced80cfb3a6401f043
Lars Kruijssen <lmapkrui@avans.nl>
2019-04-08 14:19:42 +0200

    Fixed Menu's and deleted layouts

--- resources/views/layouts/Back.blade.php
+++ resources/views/layouts/Back.blade.php
@@ -10,28 +10,30 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- <script src="{{ asset('js/App.js') }}" defer></script> --}}
+    <script src="{{ asset('js/cmsApp.js') }}" defer></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
-    
+    <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js" defer></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/lang-all.js" defer></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/Custom_Back.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
 </head>
 <body>
     <div id="app">
-        @include('inc.nav-cms')
-        <div class="row" id="body-row">
-            @include('inc.nav-cms-left')
-                <main class="py-3 col">
-                        @include('inc.messages')
-                        @yield('content')
-                </main>
+        @include('inc.Navigation.Back_Nav')
+        @include('inc.messages')
+
+        <div class="Content">
+            @yield('content')
         </div>
     </div>
 </body>
