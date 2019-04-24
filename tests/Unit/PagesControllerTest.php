b2bab77ec8c83ecf569fee321492e0806d1abdca
ivovandaalen <32672367+ivovandaalen@users.noreply.github.com>
2019-04-24 09:38:50 +0200

    Fixed tests

--- tests/Unit/PagesControllerTest.php
+++ tests/Unit/PagesControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -21,9 +23,17 @@ class PagesControllerTest extends TestCase
     }
 
     public function test_view_cmsHome_is_returned(){
+        $user = new User([
+           'role' => 'Beheerder'
+        ]);
+        $response = $this->actingAs($user)->get('/cmsHome');
+
+        $response->assertViewIs('dashPages.cmsHome');
+    }
+
+    public function cannot_get_cms_without_login(){
         $response = $this->get('/cmsHome');
 
-        $response->assertSuccessful();
-        $response->assertViewIs('dashPages.cmsHome');
+        $response->assertRedirect('/login');
     }
 }
