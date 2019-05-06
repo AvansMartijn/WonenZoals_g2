377ef6ca1dfa8ce8cd8f990f3dec8b01e6a54d08
Lars Kruijssen <lmapkrui@avans.nl>
2019-05-06 18:43:36 +0200

    Changed Create Activity

--- resources/views/dashPages/agendaCreateActivity.blade.php
+++ resources/views/dashPages/agendaCreateActivity.blade.php
@@ -1,9 +1,100 @@
 @extends('layouts.Back')
 
 @section('content')
+{{-- Page Header --}}
+<div class="BackHeader">
+    <a href="/dashboard/agenda" class="btn btn-primary"><i class="fas fa-caret-left"></i> Terug</a>
+    <h3>Activiteit aanmaken</h3>
+    <hr>
+</div>
+
+{{-- Content --}}
+{!! Form::open(['action' => 'EventsController@addEvent', 'methode' => 'POST']) !!}
+
+<div class="row justify-content-center">
+    <div class="col-md-4">
+        <h1>Gegevens</h1>
+        <hr>
+
+        <div class="form-group">
+            <input type="text" class="form-control" name="eventname" placeholder="Activiteit naam">
+        </div>
+
+        <div class="form-group">
+            <input type="text" class="form-control" name="location" placeholder="Locatie">
+        </div>
+        
+        <div class="form-group">
+            <input type="text" class="form-control" name="transport" placeholder="Vervoer">
+        </div>
+
+        <div class="form-group">
+            <textarea class="form-control" name="description" rows="4" placeholder="Beschrijving"></textarea>
+        </div>
+
+        <h3>Datum</h3>
+        <hr>
+        <div class="form-group">
+            <label for="date">Aanvang</label>
+            <input type="datetime-local" class="form-control" name="date" placeholder="Dag-Maand-Jaar Uur:Minuut">
+        </div>
+
+        <div class="form-group">
+            <label for="enddate">Eind tijd</label>
+            <input type="datetime-local" class="form-control" name="enddate" placeholder="Dag-Maand-Jaar Uur:Minuut">
+        </div>
+
+    </div>
+    
+    <div class="col-md-3">
+        <div class="funkyradio">
+            <h1>Aanmelden</h1>
+            <hr>
+
+            <div class="funkyradio-success">
+                <input type="checkbox" name="role_check[]" value="beheerder" id="Beheerder">
+                <label for="Beheerder">Beheerders</label>
+            </div>
+    
+            <div class="funkyradio-success">
+                <input type="checkbox" name="role_check[]" value="bewoner" id="Bewoner">
+                <label for="Bewoner">Bewoners</label>
+            </div>
+    
+            <div class="funkyradio-success">
+                <input type="checkbox" name="role_check[]" value="ouder" id="Ouder">
+                <label for="Ouder">Ouders</label>
+            </div>
+    
+            <div class="funkyradio-success">
+                <input type="checkbox" name="role_check[]" value="vrijwilliger" id="Vrijwilliger">
+                <label for="Vrijwilliger">Vrijwilligers</label>
+            </div>
+
+            <h3>Aanwezig melden</h3>
+            <hr>
+            <div class="funkyradio-success">
+                <input type="checkbox" name="auto_apply" value="auto_apply" id="Auto_apply">
+                <label for="Auto_apply">Gebruikers automatisch aanmelden</label>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="col-md-7">
+        <hr>
+        {{Form::submit("Activiteit aanmaken", ['class' => 'btn btn-success full-width'])}}
+
+        {!! Form::close() !!}
+    </div>
+    
+</div>
+
+
+{{-- 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-7">
             <div class="card bottom-spacer">
                 <div class="card-header">Activiteit aanmaken</div>
 
@@ -84,5 +175,5 @@
        
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
--- resources/views/dashPages/agendaOverview.blade.php
+++ resources/views/dashPages/agendaOverview.blade.php
@@ -1,40 +1,37 @@
 @extends('layouts.Back')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-                <div class="bottom-spacer">
-                        <a href="/dashboard" class="btn btn-primary">Terug</a>
-                        
-                        @foreach (Auth::user()->authorizations as $userauthorization)
-                        {{-- show agenda --}}
-                        @if ($userauthorization->authorization == "Activiteit")
-                        
-                        <a href="/dashboard/agenda/create/activity" class="btn btn-success">Nieuwe activiteit</a>
-                        <a href="/dashboard/agenda/create/meal" class="btn btn-success">Nieuwe maaltijd activiteit</a>
-                        @endif
-                        @endforeach
-                    </div>
-            <div class="card">
-                <div class="card-header">Agenda</div>
+@section('content') 
+{{-- Page Header --}}
+<div class="BackHeader">
+    <a href="/dashboard" class="btn btn-primary">Terug</a>
+    <h3>Agenda</h3>
+    <hr>
+</div>
 
-<div class="Calender">
-    <div class="BackHeader">
-        <h3><i class="fas fa-calendar"></i> Agenda</h3>
-        <hr>
-    </div>
+{{-- Content --}}
+<div class="row justify-content-center">
+    <div class="col-md-7">
 
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
+        {{-- Buttons --}}
+        @foreach (Auth::user()->authorizations as $userauthorization)
+            @if ($userauthorization->authorization == "Activiteit")
+                <div class="center">
+                    <a href="/dashboard/agenda/create/activity" class="btn btn-success half-width">Nieuwe activiteit</a>
+                    <a href="/dashboard/agenda/create/meal" class="btn btn-success half-width">Nieuwe maaltijd activiteit</a>
                 </div>
             @endif
-            {!! $calendar_details->calendar() !!}
-            {!! $calendar_details->script() !!}
-        </div>
+        @endforeach
+        <hr>
+
+        {{-- Calender --}}
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        {!! $calendar_details->calendar() !!}
+        {!! $calendar_details->script() !!}
     </div>
 </div>
 
