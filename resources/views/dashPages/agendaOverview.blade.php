0880c3261d3d46290620e667a586df54523687f2
Lars Kruijssen <lmapkrui@avans.nl>
2019-05-06 14:29:35 +0200

    Updated agend overview page

--- resources/views/dashPages/agendaOverview.blade.php
+++ resources/views/dashPages/agendaOverview.blade.php
@@ -7,29 +7,18 @@
         <h3>Agenda</h3>
         <hr>
     </div>
-    {!! $calendar_details->calendar() !!}
-    {!! $calendar_details->script() !!}
-</div>
-
-{{-- 
-<div class="container">
+    
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Agenda</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                        {!! $calendar_details->calendar() !!}
-                        {!! $calendar_details->script() !!}
-
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+            {!! $calendar_details->calendar() !!}
+            {!! $calendar_details->script() !!}
         </div>
     </div>
-</div> --}}
+</div>
+
 @endsection
