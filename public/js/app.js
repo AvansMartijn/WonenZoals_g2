5897f6869da5d3eb1fd0f8361b15e7e8421577c3
Lars Kruijssen <lmapkrui@avans.nl>
2019-05-11 20:55:47 +0200

    fixed search

--- public/js/app.js
+++ public/js/app.js
@@ -48941,15 +48941,8 @@ $(function () {
 $(function () {
   $('#Search').keyup(function () {
     var value = $(this).val();
-    var patt = new RegExp(value, "i");
-    $('.Searchable').find('tr').each(function () {
-      if (!($(this).find('td').text().search(patt) >= 0)) {
-        $(this).not('.myHead').hide();
-      }
-
-      if ($(this).find('td').text().search(patt) >= 0) {
-        $(this).show();
-      }
+    $(".Searchable tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     });
   });
 });
