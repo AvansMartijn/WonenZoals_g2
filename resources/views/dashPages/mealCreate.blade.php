1f8abee416f31a8193db4fa7a1069266009d9729
Lars Kruijssen <lmapkrui@avans.nl>
2019-05-11 16:54:30 +0200

    Bugfixes +edited dashboards

--- resources/views/dashPages/mealCreate.blade.php
+++ resources/views/dashPages/mealCreate.blade.php
@@ -1,45 +1,62 @@
 @extends('layouts.Back')
 
 @section('content')
+{{-- Page Header --}}
+<div class="BackHeader">
+    <a href="/dashboard/maaltijden" class="btn btn-primary"><i class="fas fa-caret-left"></i> Terug</a>
+    <h3>Gerecht Aanmaken</h3>
+    <hr>
+</div>
+
+<div class="HamburgerMenu">
+    <a><i class="fas fa-bars"></i> Menu</a>
+</div>
+
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <a href="/dashboard/maaltijden" class="btn btn-primary">Terug</a>
-            <div class="card bottom-spacer">
-                <div class="card-header">Gerecht aanmaken</div>
+    {!! Form::open(['action' => 'MealsController@store', 'methode' => 'POST']) !!}
 
-                <div class="card-body">
-                       
-                    {!! Form::open(['action' => 'MealsController@store', 'methode' => 'POST']) !!}
-                                    
-                                <div class="form-group">
-                                    <label for="mealname">Gerecht naam</label>
-                                    <input type="text" class="form-control" name="mealname" placeholder="gerecht naam">
-                                </div>
+    <div class="MainContent">
+        <h1>Gerecht</h1>
+        <hr>
 
-                                <div class="form-group">
-                                    <label for="description">Beschrijving</label>
-                                    <textarea class="form-control" name="description" rows="3" placeholder="beschrijving"></textarea>
-                                </div>
+        <div class="form-group">
+            <label for="mealname">Gerecht naam</label>
+            <input type="text" class="form-control" name="mealname" placeholder="gerecht naam">
+        </div>
 
-                                <div class="radio">
-                                    <label><input type="radio" name="gerechttype" value="voorgerecht" checked>Voorgerecht</label>
-                                </div>
-                                <div class="radio">
-                                    <label><input type="radio" name="gerechttype" value="hoofdgerecht">Hoofdgerecht</label>
-                                </div>
-                                <div class="radio">
-                                    <label><input type="radio" name="gerechttype" value="nagerecht">Nagerecht</label>
-                                </div>
-
-                                {{Form::submit("Versturen", ['class' => 'btn btn-success'])}}
-
-                                {!! Form::close() !!}
-                </div>
-            </div>
-
-       
+        <div class="form-group">
+            <label for="description">Beschrijving</label>
+            <textarea class="form-control" name="description" rows="3" placeholder="beschrijving"></textarea>
         </div>
     </div>
+
+    <div class="SideContent">
+        <h1>Gerecht Type</h1>
+        <hr>
+        
+        <div class="funkyradio">
+            <div class="funkyradio-success">
+                <input type="radio" name="gerechttype" value="voorgerecht" id="voorgerecht" />
+                <label for="voorgerecht">Voorgerecht</label>
+            </div>
+
+            <div class="funkyradio-success">
+                <input type="radio" name="gerechttype" value="hoofdgerecht" id="hoofdgerecht" />
+                <label for="hoofdgerecht">hoofdgerecht</label>
+            </div>
+
+            <div class="funkyradio-success">
+                <input type="radio" name="gerechttype" value="nagerecht" id="nagerecht" />
+                <label for="nagerecht">nagerecht</label>
+            </div>
+        </div>
+    </div>
+
+    <div class="MainContent">
+        <hr>
+        {{Form::submit("Versturen", ['class' => 'btn btn-success full-width'])}}
+    </div>
+    {!! Form::close() !!}
 </div>
+
 @endsection
--- resources/views/dashPages/mealDetail.blade.php
+++ resources/views/dashPages/mealDetail.blade.php
@@ -3,33 +3,31 @@
 @section('content')
 {{-- Page Header --}}
 <div class="BackHeader">
-        <a href="/dashboard/maaltijden" class="btn btn-primary"><i class="fas fa-caret-left"></i> Terug</a>
-        <h3>Gerecht details</h3>
-        <hr>
-    </div>
-    
-    <div class="HamburgerMenu">
-        <a><i class="fas fa-bars"></i> Menu</a>
-    </div>
- 
+    <a href="/dashboard/maaltijden" class="btn btn-primary"><i class="fas fa-caret-left"></i> Terug</a>
+    <h3>Gerecht details</h3>
+    <hr>
+</div>
 
-    <div class="container">
-        <div class="MainContent">
-            <div class="CustomCardContent">
-                <h1>{{$meal->name}}</h1>
-                <hr>
+<div class="HamburgerMenu">
+    <a><i class="fas fa-bars"></i> Menu</a>
+</div>
 
-                <p>
-                    <b>Type gerecht:</b> {{$meal->type}}
-                </p>
-                <p>
-                    <b>Beschrijving:</b><br> {{$meal->description}}
-                </p>
-              
-            </div>
+
+<div class="container">
+    <div class="MainContent">
+        <div class="CustomCardContent">
+            <h1>{{$meal->name}}</h1>
+            <hr>
+
+            <p>
+                <b>Type gerecht:</b> {{$meal->type}}
+            </p>
+            <p>
+                <b>Beschrijving:</b><br> {{$meal->description}}
+            </p>
+            
         </div>
     </div>
-
 </div>
 
 @endsection
