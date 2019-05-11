2683f0f3a18b2198208ee1b1f8970c84c86efc28
Lars Kruijssen <lmapkrui@avans.nl>
2019-05-11 18:47:50 +0200

    Started Messages Refactor

--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,19 +1,40 @@
-@if(count($errors) > 0)
-    @foreach($errors->all() as $error)
-        <div class="alert alert-danger">
-            {{$error}}
-        </div>
-    @endforeach
-@endif
+<script>
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "2000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
 
-@if(session('success'))
-    <div class="alert alert-success">
-        {{session('success')}}
-    </div>
-@endif
-
-@if(session('error'))
-    <div class="alert alert-danger">
-        {{session('error')}}
-    </div>
-@endif
+    @if(Session::has('message'))
+            var type = "{{ Session::get('alert-type', 'info') }}";
+            switch(type){
+            case 'info':
+                    toastr.info("{{ Session::get('message') }}");
+                    break;
+            
+            case 'warning':
+                    toastr.warning("{{ Session::get('message') }}");
+                    break;
+    
+            case 'success':
+                    toastr.success("{{ Session::get('message') }}");
+                    break;
+    
+            case 'error':
+                    toastr.error("{{ Session::get('message') }}");
+                    break;
+            }
+    @endif
+</script>
