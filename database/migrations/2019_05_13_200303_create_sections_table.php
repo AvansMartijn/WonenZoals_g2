73be4ffb346edc1a44e22e03834fb48d7e48f108
AvansChiel <cgmarts@avans.nl>
2019-05-13 22:46:33 +0200

    added migration + seed

---
+++ database/migrations/2019_05_13_200303_create_sections_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('section_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->timestamps();
+        });
+
+        Schema::create('sections', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order')->unsigned();
+            $table->string('name');
+            $table->string('content')->nullable();
+            $table->integer('type_id')->unsigned();
+            $table->boolean('default_section');
+            $table->string('img_url')->nullable();
+
+            $table->foreign('type_id')
+            ->references('id')
+            ->on('section_types')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+
+        Schema::create('default_sections', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order')->unsigned();
+            $table->string('name');
+            $table->string('content');
+            $table->integer('type_id')->unsigned();
+            $table->boolean('default_section');
+
+            $table->foreign('type_id')
+            ->references('id')
+            ->on('section_types')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sections');
+        Schema::dropIfExists('default_sections');
+        Schema::dropIfExists('section_types');
+    }
+}
---
+++ database/seeds/SectionsSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SectionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('default_sections')->delete();
+        DB::table('sections')->delete();
+        DB::table('section_types')->delete();
+
+        $section_types = [
+            ['id' => 1, 'type' => 'leaf'],
+            ['id' => 2, 'type' => 'seperator'],
+            ['id' => 3, 'type' => 'text'],
+            ['id' => 4, 'type' => 'residents'],
+            ['id' => 5, 'type' => 'news'],
+            ['id' => 6, 'type' => 'contact'],
+            ['id' => 7, 'type' => 'sponsors']
+            
+        ];
+
+        foreach($section_types as $type){
+            Section::create($type);
+        };
+
+        $sections = [
+            ['id' => 1, 'order' => 1, 'name' => 'leaf', 'content' => '!!!!!!!!!!!', 'type_id' => 1, 'default_section' => 1],
+            ['id' => 2, 'order' => 2, 'name' => 'stichting zoals', 'content' => '!!!!!!!!!!!', 'type_id' => 3, 'default_section' => 1],
+            ['id' => 3, 'order' => 3, 'name' => 'scheiding', 'content' => '!!!!!!!!!!!', 'type_id' => 2, 'default_section' => 1],
+            ['id' => 4, 'order' => 4, 'name' => 'bewoners', 'content' => '!!!!!!!!!!!', 'type_id' => 4, 'default_section' => 1],
+            ['id' => 5, 'order' => 5, 'name' => 'ondersteuning', 'content' => '!!!!!!!!!!!', 'type_id' => 3, 'default_section' => 1],
+            ['id' => 6, 'order' => 6, 'name' => 'scheiding', 'content' => '!!!!!!!!!!!', 'type_id' => 2, 'default_section' => 1],
+            ['id' => 7, 'order' => 7, 'name' => 'nieuws', 'content' => '!!!!!!!!!!!', 'type_id' => 5, 'default_section' => 1],
+            ['id' => 8, 'order' => 8, 'name' => 'contact', 'content' => '!!!!!!!!!!!', 'type_id' => 6, 'default_section' => 1],
+            ['id' => 9, 'order' => 9, 'name' => 'sponsors', 'content' => '!!!!!!!!!!!', 'type_id' => 7, 'default_section' => 1]
+            
+        ];
+
+        foreach($sections as $section){
+            Section::create($sections);
+            DefaultSection::create($sections);
+        };
+    }
+}
