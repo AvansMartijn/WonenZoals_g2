0466de084b72ed375939abdb89b4d9dd0fb016bf
AvansMartijn <mfghaneg@avans.nl>
2019-05-14 13:14:56 +0200

    frapant toegevoegd

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -506,7 +506,7 @@
                         <a href="https://www.yellenyonkers.nl/" target="_blank"><img class="img-fluid" src="{{ asset('img/LogoYY_2015.png') }}" alt=""></a>
                     </div>
                     <div class="col">
-                        <a href="https://www.mijneigenthuis.eu/"><img class="img-fluid" src="{{ asset('img/roodborstje.png') }}" alt=""></a>
+                        <a href="https://www.fra-pant.nl"><img class="img-fluid" src="{{ asset('img/roodborstje.png') }}" alt=""></a>
                     </div>
                 </div>
             </div>
