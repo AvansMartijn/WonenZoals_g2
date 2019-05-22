5c99c4d158269626783f16750f3e98cc5d8ff164
Xandor Janssen <xpjh.janssen@student.avans.nl>
2019-05-22 15:44:51 +0200

    forum + dashboard

--- resources/views/dashPages/dashBeheerder.blade.php
+++ resources/views/dashPages/dashBeheerder.blade.php
@@ -15,7 +15,42 @@
 <div class="container">
     <div class="ContentMainFull">
         <h1>Welkom {{Auth::user()->name}}</h1>
-        Binnenkort Beschikbaar...
+        Underconstruction
+        <br>
+        <hr>
+        <h2>Contact formulier KPI</h2>
+
+        <p>Aantal ingevulde contact formulieren: {{count($contacts)}} </p>
+        <p>Aantal ingevulde contact formulieren afgelopen 30 dagen: {{count($contacts30)}}</p>
+        <table class="table table-striped">
+            <tr>
+                <th>naam</th>
+                <th>email</th>
+                <th>onderwerp</th>
+                <th>bericht</th>
+            </tr>
+
+                @if (count($contacts)>0)
+                                                
+                        @foreach ($contacts as $contact)
+                        <tr>
+                                <td>{{$contact->name}}</td>
+                                <td>{{$contact->email}}</td>
+                                <td>{{$contact->subject}}</td>
+                                <td>{{$contact->message}}</td>
+                        </tr>
+                        @endforeach
+
+                        
+                        
+                    @else
+                    <tr>
+                        <td colspan="4">Er zijn ingevulde contactformulieren</td>
+                    </tr>
+
+                @endif
+            </table>  
+            
     </div>
 </div>
 
