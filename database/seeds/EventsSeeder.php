27f7c8fada3f1e1b9e1fd2c00284d559ab03ba33
Xandor Janssen <xpjh.janssen@student.avans.nl>
2019-05-23 15:41:10 +0200

    seed data update

--- database/seeds/EventsSeeder.php
+++ database/seeds/EventsSeeder.php
@@ -35,7 +35,14 @@ class EventsSeeder extends Seeder
             ['id' => 3, 'user_id' => 2, 'event_id' => 4],
             ['id' => 4, 'user_id' => 5, 'event_id' => 1],
             ['id' => 5, 'user_id' => 6, 'event_id' => 1],
-            ['id' => 6, 'user_id' => 7, 'event_id' => 1]
+            ['id' => 6, 'user_id' => 7, 'event_id' => 1],
+
+            ['id' => 7, 'user_id' => 1, 'event_id' => 1, ],
+            ['id' => 8, 'user_id' => 1, 'event_id' => 2],
+            ['id' => 9, 'user_id' => 1, 'event_id' => 3],
+            ['id' => 10, 'user_id' => 1, 'event_id' => 4],
+            ['id' => 11, 'user_id' => 1, 'event_id' => 5]
+
         ];
 
 
