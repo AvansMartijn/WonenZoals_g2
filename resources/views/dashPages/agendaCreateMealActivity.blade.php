dfaa65939122fba464cb56a8b82b604df002226a
Lars Kruijssen <lmapkrui@avans.nl>
2019-05-23 12:04:50 +0200

    Update agendaCreateMealActivity.blade.php

--- resources/views/dashPages/agendaCreateMealActivity.blade.php
+++ resources/views/dashPages/agendaCreateMealActivity.blade.php
@@ -100,10 +100,7 @@
                 <h1>Aanmelden</h1>
                 <hr>
                 
-                {{-- <div class="funkyradio-success"> --}}
-                    <input type="hidden" name="role_check[]" value="beheerder" id="Beheerder" checked>
-                    {{-- <label for="Beheerder">Beheerders</label> --}}
-                {{-- </div> --}}
+                <input type="hidden" name="role_check[]" value="beheerder" id="Beheerder" checked>
         
                 <div class="funkyradio-success">
                     <input type="checkbox" name="role_check[]" value="bewoner" id="Bewoner">
