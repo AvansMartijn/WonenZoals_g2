b3ff58b250459e256d0cafce72126e1387772386
Lars Kruijssen <lmapkrui@avans.nl>
2019-06-16 15:31:09 +0200

    KUT PROJECT

--- resources/views/dashPages/mealDetail.blade.php
+++ resources/views/dashPages/mealDetail.blade.php
@@ -5,7 +5,6 @@
 <div class="BackHeader">
     <a href="/dashboard/maaltijden" class="btn btn-primary"><i class="fas fa-caret-left"></i> Terug</a>
     <h3>Gerecht Details</h3>
-    <a href="/dashboard/maaltijden/edit/{{$meal->id}}" class="btn btn-primary"><i class="fas fa-caret-left"></i> Bewerken</a>
     <hr>
 </div>
 
@@ -19,16 +18,17 @@
         <div class="CustomCardContent">
             <h1>{{$meal->name}}</h1>
             <hr>
-
             <p>
                 <b>Type Gerecht:</b> {{$meal->type}}
             </p>
             <p>
                 <b>Beschrijving:</b><br> {{$meal->description}}
             </p>
-            
+            <a href="/dashboard/maaltijden/edit/{{$meal->id}}" class="btn btn-primary">Bewerken</a>
         </div>
     </div>
+
+    
     @if ($meal->img_url != null && $meal->img_url != "")
     <div class="SideContent">
         <div class="CustomCardContent">
